e4299b5188146d200ebb09489ba82de187495eb3
Jerome Bakker <jeabakker@coldtrick.com>
2018-12-14 15:05:34 +0100

    added: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-14 15:03
+ */
+
+return array (
+  'dataroot_browser' => 'Dataroot Browser',
+  'admin:administer_utilities:dataroot_browser' => 'Dataroot Browser',
+  'dataroot_browser:menu:user_hover' => 'Bekijk dataroot',
+  'dataroot_browser:list:root_dir' => 'Dataroot',
+  'dataroot_browser:list:invalid_dir' => 'Ongeldige map. Begin opnieuw met bladeren',
+  'dataroot_browser:list:name' => 'Naam',
+  'dataroot_browser:list:size' => 'Grootte',
+  'dataroot_browser:list:owner' => 'Eigenaar',
+  'dataroot_browser:list:writeable' => 'Beschrijfbaar',
+  'dataroot_browser:list:modified' => 'Laatste wijziging',
+  'dataroot_browser:list:no_content' => 'Geen bestanden of mappen gevonden',
+  'dataroot_browser:actions:delete_file:error:input' => 'Ongeldige invoer om een bestand te kunnen verwijderen',
+  'dataroot_browser:actions:delete_file:error:exists' => 'De bestandsnaam bestaat niet op het systeem, of is een map',
+  'dataroot_browser:actions:delete_file:error:delete' => 'Er is een onbekende fout opgetreden tijdens het verwijderen van het bestand',
+  'dataroot_browser:actions:delete_file:success' => 'Het bestand is succesvol verwijderd',
+);
